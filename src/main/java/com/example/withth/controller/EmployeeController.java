f66179da99da7a5edc8517e5092935eed5e155f6
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-07-14 08:28:30 +0300

    feat: adding styling

--- src/main/java/com/example/withth/controller/EmployeeController.java
+++ src/main/java/com/example/withth/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.example.withth.controller;
 
 import com.example.withth.model.Employee;
+import com.example.withth.repository.EmployeeRepository;
 import com.example.withth.service.EmployeeService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +19,7 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
     private final EmployeeService service;
 
     @ModelAttribute("employeeList")
@@ -47,7 +48,15 @@ public class EmployeeController {
         service.save(employee);
         return "redirect:/";
     }
-
+    @PostMapping(value="/edit")
+    public String updateEmployee(@ModelAttribute Employee employee, @ModelAttribute("file") MultipartFile file) throws IOException {
+        employee.setProfilePicture(file.getOriginalFilename());
+        employee.setContent(file.getBytes());
+        employee.setSize(file.getSize());
+        System.out.println(employee);
+        service.save(employee);
+        return "redirect:/";
+    }
 
     @GetMapping("/image")
     public void showImage(@Param("id") Long id, HttpServletResponse response, Employee employee) throws IOException {
@@ -64,10 +73,11 @@ public class EmployeeController {
         return modelAndView;
     }
 
-//    @GetMapping(value = "/edit-employee-form")
-//    public ModelAndView editEmployeeForm(@RequestParam(name = "employeeId") Integer id) {
-//        ModelAndView modelAndView = new ModelAndView("employee-form");
-//        modelAndView.addObject("employee", new Employee(id, "boo", null));
-//        return modelAndView;
-//    }
+    @GetMapping("/edit")
+    public ModelAndView editEmployeeForm(@Param("id") Long id) {
+        Employee employee = service.findById(id);
+        ModelAndView modelAndView = new ModelAndView("edit");
+        modelAndView.addObject("employee", employee);
+        return modelAndView;
+    }
 }
