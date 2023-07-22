9cc386d4004478aac40473c56eabc50db01ffd01
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-22 11:12:06 +0300

    chore: implement position validator

---
+++ src/main/java/com/example/prog4/controller/validator/PositionValidator.java
@@ -0,0 +1,19 @@
+package com.example.prog4.controller.validator;
+
+import com.example.prog4.controller.validator.utils.StringValidator;
+import com.example.prog4.model.exception.BadRequestException;
+import com.example.prog4.repository.entity.Position;
+
+public class PositionValidator {
+    public void validate(Position position){
+        StringBuilder error = new StringBuilder();
+
+        if(StringValidator.isNotNullAndNotBlank(position.getName())){
+            error.append("Position name is mandatory");
+        }
+
+        if(!error.isEmpty()){
+            throw new BadRequestException(error.toString());
+        }
+    }
+}
