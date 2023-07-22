b1e0cba49dc2173384fbb42186e1836958ab9c7a
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-22 08:54:08 +0300

    chore: update migration

--- src/main/resources/db/migration/V0_3__Update_employe_table.sql
+++ src/main/resources/db/migration/V0_3__Update_employe_table.sql
@@ -26,14 +26,14 @@ CREATE TABLE IF NOT EXISTS "have_position"
 );
 
 ALTER TABLE "employee"
-    add column phone             varchar,
-    add column address           varchar,
+    add column phone              varchar,
+    add column address            varchar,
     add column professional_email varchar,
     add column personal_email     varchar,
-    add column cin               varchar,
-    add column cnaps             varchar,
+    add column cin                varchar,
+    add column cnaps              varchar,
     add column children_number    integer,
-    add column entrance_date     date,
-    add column departure_date    date,
-    add column sex               sex,
-    add column csp               csp;
+    add column entrance_date      date,
+    add column departure_date     date,
+    add column sex                sex,
+    add column csp                csp;
