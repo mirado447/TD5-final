06d220895e0680afbac0477f91575caf9e63b3b2
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-22 09:05:04 +0300

    chore: update models

--- src/main/java/com/example/prog4/model/Employee.java
+++ src/main/java/com/example/prog4/model/Employee.java
@@ -1,5 +1,8 @@
 package com.example.prog4.model;
 
+import com.example.prog4.repository.entity.Position;
+import com.example.prog4.repository.entity.enums.Csp;
+import com.example.prog4.repository.entity.enums.Sex;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,8 +23,9 @@ public class Employee implements Serializable {
     private String cnaps;
     private Integer childNumber;
     private Email email;
-    private NameDescription socioeconomicGroup;
-    private List<NameDescription> position;
+    private Csp csp;
+    private Sex sex;
+    private List<Position> positions;
 
     //dates 
     private LocalDate birthDate;
