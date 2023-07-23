4453c308f9b59eaf8fc6a646be2c27aec7b45ccd
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-23 18:42:09 +0300

    chore: update model

---
+++ src/main/java/com/example/prog4/model/enums/EmployeeSortField.java
@@ -0,0 +1,12 @@
+package com.example.prog4.model.enums;
+
+import lombok.Getter;
+
+public enum EmployeeSortField {
+    firstName,
+    lastName,
+    birthDate,
+    childrenNumber,
+    entranceDate,
+    departureDate;
+}
--- src/main/java/com/example/prog4/model/utilities/PerPage.java
+++ src/main/java/com/example/prog4/model/utilities/PerPage.java
@@ -1,16 +1,13 @@
-package com.example.prog4.model;
+package com.example.prog4.model.utilities;
 
 import com.example.prog4.model.exception.BadRequestException;
 import lombok.Getter;
 
 public record PerPage(@Getter Integer perPage) {
-    private static final int DEFAULT_PER_PAGE = 10;
-    private static final int MAX_PER_PAGE = 500;
+    public static final int MAX_PER_PAGE = 500;
 
     public PerPage(Integer perPage) {
-        if (perPage == null) {
-            this.perPage = DEFAULT_PER_PAGE;
-        } else if (perPage > MAX_PER_PAGE) {
+        if (perPage > MAX_PER_PAGE) {
             throw new BadRequestException("Page size must be < " + MAX_PER_PAGE);
         } else {
             this.perPage = perPage;
