af0e11fe921262996406309cec59aa002d5e5632
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-23 18:41:51 +0300

    chore: update converter

--- src/main/java/com/example/prog4/controller/converter/PerPageConverter.java
+++ src/main/java/com/example/prog4/controller/converter/PerPageConverter.java
@@ -1,6 +1,6 @@
 package com.example.prog4.controller.converter;
 
-import com.example.prog4.model.PerPage;
+import com.example.prog4.model.utilities.PerPage;
 import org.springframework.core.convert.converter.Converter;
 
 public class PerPageConverter implements Converter<String, PerPage> {
