803ec870b7cc70b341f15c8f9eb38621c2a9eaef
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-07-26 09:08:10 +0300

    feat: employee request

---
+++ src/main/java/com/example/withth/controller/request/EmployeeFilter.java
@@ -0,0 +1,20 @@
+package com.example.withth.controller.request;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EmployeeFilter {
+    private String name;
+    private String function;
+    private String sex;
+    private LocalDate entryDate;
+    private LocalDate departureDate;
+    private String orderBy;
+    private String direction;
+}
