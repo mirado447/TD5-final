fde2bcbcdcec49806069ff64944eba6302f32588
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-07-31 23:20:34 +0300

    feat: LoginController

---
+++ src/main/java/com/example/withth/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.example.withth.controller;
+
+import com.example.withth.controller.request.LoginDetails;
+import com.example.withth.models.entity.Employee;
+import com.example.withth.repository.EmployeeRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class LoginController {
+    private final EmployeeRepository repository;
+
+    public LoginController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("AuthUsername");
+        session.removeAttribute("AuthPassword");
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginDetails loginDetails, HttpSession session) {
+        List<Employee> allByNameAndPasswordEquals = repository.findAllByPasswordAndName(loginDetails.getPassword(), loginDetails.getUsername());
+        if (allByNameAndPasswordEquals.isEmpty()){
+            return "redirect:/login";
+        }
+        Employee employee = allByNameAndPasswordEquals.get(0);
+        session.setAttribute("AuthUsername", employee.getName());
+        session.setAttribute("AuthPassword", employee.getPassword());
+        return "redirect:/";
+    }
+}
