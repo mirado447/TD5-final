19f24aaa9666a64b0490419479758546911e01e6
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-01 08:26:28 +0300

    fix: pic update

--- src/main/java/com/example/withth/models/model/EmployeeModel.java
+++ src/main/java/com/example/withth/models/model/EmployeeModel.java
@@ -1,11 +1,14 @@
 package com.example.withth.models.model;
 
 import com.example.withth.models.entity.Phone;
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
