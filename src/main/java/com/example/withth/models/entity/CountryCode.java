c6d6fb5b4087be3690be2d65098ea89ae885c225
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-01 09:49:11 +0300

    feat: add country code

---
+++ src/main/java/com/example/withth/models/entity/CountryCode.java
@@ -0,0 +1,25 @@
+package com.example.withth.models.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Builder
+public class CountryCode implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(unique = true)
+    private String content;
+    @OneToMany(mappedBy = "countryCode")
+    private Collection<Phone> phone;
+}
