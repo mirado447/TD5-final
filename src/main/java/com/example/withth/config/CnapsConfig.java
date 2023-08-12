76f34e26cfc1f784f021d3c8980ddece4ad0f349
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-12 21:07:31 +0300

    feat: add cnaps entity manager factory

--- src/main/java/com/example/withth/config/CnapsConfig.java
+++ src/main/java/com/example/withth/config/CnapsConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.Objects;
 
 @Configuration
@@ -36,5 +40,23 @@ public class CnapsConfig {
         dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("cnaps.datasource.driver-class-name")));
         return dataSource;
     }
+
+    @Bean("cnapsEntityManagerFactory")
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        var entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManager.setJpaVendorAdapter(vendorAdapter);
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto",
+                environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect",
+                environment.getProperty("hibernate.dialect"));
+        entityManager.setJpaPropertyMap(properties);
+        return entityManager;
+    }
+
 
 }
