7149afdea98d7a30fcf7ce2b17c1e7b2a5c923e4
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-13 13:09:47 +0300

    feat(db-conf): adding employee management db conf

---
+++ src/main/java/com/example/withth/config/EmployeeManagementConfig.java
@@ -0,0 +1,67 @@
+package com.example.withth.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "employeeManagementEntityManagerFactory",
+        basePackages = {"com.example.withth.repository.employeeManagement"},
+        transactionManagerRef = "employeeManagementTransactionManager"
+)
+public class EmployeeManagementConfig {
+    private final Environment env;
+
+    public EmployeeManagementConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean(name = "employeeManagementDataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("employeeManagement.datasource.driver-class-name")));
+        dataSource.setUrl(env.getProperty("employeeManagement.datasource.url"));
+        dataSource.setUsername(env.getProperty("employeeManagement.datasource.username"));
+        dataSource.setPassword(env.getProperty("employeeManagement.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean("employeeManagementEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        var entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManager.setJpaVendorAdapter(vendorAdapter);
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("employeeManagement.jpa.hibernate.ddl-auto"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        entityManager.setJpaPropertyMap(properties);
+        entityManager.setPackagesToScan("com.example.withth.models.employeeManagement");
+        return entityManager;
+    }
+
+    @Bean("employeeManagementTransactionManager")
+    public PlatformTransactionManager employeeTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+        return transactionManager;
+    }
+}
