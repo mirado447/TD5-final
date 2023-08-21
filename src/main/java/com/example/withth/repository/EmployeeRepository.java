700bc156d4f2e8dc387ff7bcaa81b4b4654fa6be
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-21 14:05:53 +0300

    fix: returning local employee instead of cnaps employee

--- src/main/java/com/example/withth/repository/EmployeeRepository.java
+++ src/main/java/com/example/withth/repository/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.example.withth.repository;
 
 
-import com.example.withth.models.cnaps.Employee;
+import com.example.withth.models.employeeManagement.entity.Employee;
 
 public interface EmployeeRepository {
     public Employee findById(Long id);
