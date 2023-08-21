8e7ebfc0fc8e5342e0b44db6fac872713dea0355
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-21 16:39:21 +0300

    refactor: removing repository annotation on employee repo and cnaps repo

--- src/main/java/com/example/withth/repository/cnaps/jpa/CnapsEmployeeRepository.java
+++ src/main/java/com/example/withth/repository/cnaps/jpa/CnapsEmployeeRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-@Repository
 public interface CnapsEmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByPasswordAndName(String password, String name);
 
